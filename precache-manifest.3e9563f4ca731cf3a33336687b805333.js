cfff3b7f5fefca7954626a8039a35a07700f1030
nshimiyimana <emmypiquel@gmail.com>
2025-05-25 16:42:41 +0200

    Updates

---
+++ precache-manifest.3e9563f4ca731cf3a33336687b805333.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2560ef179b5a96c3570e810eeff18818",
+    "url": "/electricity_pay_app/index.html"
+  },
+  {
+    "revision": "a14af74062ccbf7ff860",
+    "url": "/electricity_pay_app/static/css/main.52623ab5.chunk.css"
+  },
+  {
+    "revision": "6ea203ec8706804aa3c4",
+    "url": "/electricity_pay_app/static/js/2.1b6baee6.chunk.js"
+  },
+  {
+    "revision": "1eb98706859bae847c8762bbc3c10000",
+    "url": "/electricity_pay_app/static/js/2.1b6baee6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "766360be3772a56ece31",
+    "url": "/electricity_pay_app/static/js/3.e61b46cf.chunk.js"
+  },
+  {
+    "revision": "a14af74062ccbf7ff860",
+    "url": "/electricity_pay_app/static/js/main.b533f791.chunk.js"
+  },
+  {
+    "revision": "698be0b5cc45898410ee",
+    "url": "/electricity_pay_app/static/js/runtime-main.5dc5070e.js"
+  }
+]);
